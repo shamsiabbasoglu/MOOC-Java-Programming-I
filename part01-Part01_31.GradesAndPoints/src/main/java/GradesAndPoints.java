706ea0dc004605983b12af8af5c3c03b8a62bb50
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-03-23 22:05:37 +0400

    Conditional statements and conditional operation

---
+++ part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class GradesAndPoints {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give points [0-100]:");
+        int num = Integer.valueOf(scan.nextLine());
+
+        if (num < 0) {
+            System.out.println("Grade: " + "impossible!");
+        } else if (num > 100) {
+            System.out.println("Grade: " + "incredible!");
+        } else {
+            if (num < 50) {
+                System.out.println("Grade: " + "failed");
+            } else if (num < 60) {
+                System.out.println("Grade: " + "1");
+            } else if (num < 70) {
+                System.out.println("Grade: " + "2");
+            } else if (num < 80) {
+                System.out.println("Grade: " + "3");
+            } else if (num < 90) {
+                System.out.println("Grade: " + "4");
+            } else {
+                System.out.println("Grade: " + "5");
+            }
+        }
+    }
+}
