de8bac3ccd2f154292a681af01e90dd403e637f2
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-04-18 15:22:25 +0400

    Remove last index from a List

---
+++ Part03/part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+
+public class RemoveLast {
+
+    public static void main(String[] args) {
+        // Try your method in here
+        ArrayList<String> strings = new ArrayList<>();
+        
+        removeLast(strings);
+    }
+
+    public static void removeLast(ArrayList<String> strings) {
+        if(strings.isEmpty()) {
+            return;
+        }
+        strings.remove(strings.size() - 1);
+    }
+}
