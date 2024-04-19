a85dc982341ff7bde81c2988b15101bbe6b5b08c
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-04-19 15:39:31 +0400

    Array as a parameter of a method

---
+++ Part03/part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
@@ -0,0 +1,23 @@
+
+public class ArrayPrinter {
+
+    public static void main(String[] args) {
+        // You can test your method here
+        int[] array = {5, 1, 3, 4, 2};
+        printNeatly(array);
+    }
+
+    public static void printNeatly(int[] array) {
+        // Write some code in here
+        int index = 0;
+        while (true) {
+            if (index == array.length - 1) {
+                System.out.print(array[index]);
+                break;
+            } else {
+                System.out.print(array[index] + ", ");
+                index++;
+            }
+        }
+    }
+}
