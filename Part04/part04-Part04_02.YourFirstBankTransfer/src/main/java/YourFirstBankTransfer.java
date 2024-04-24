d635e29e06411297be2808cc10a1d461c7db8c52
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-04-24 06:04:21 +0400

    Classes and Objects

---
+++ Part04/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -0,0 +1,17 @@
+
+public class YourFirstBankTransfer {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // write your program here
+        
+        Account account1 = new Account("Matthews account", 1000);
+        Account account2 = new Account("My account", 0);
+        
+        account1.withdrawal(100);
+        account2.deposit(100);
+        
+        System.out.println(account1);
+        System.out.println(account2);
+    }
+}
