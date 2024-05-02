5a2c036d6c2a7aca56aa2e5f353c76f0f9096f1c
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-05-02 15:04:21 +0400

    Method Overloading

---
+++ Part05/part05-Part05_06.OverloadedCounter/src/main/java/Counter.java
@@ -0,0 +1,37 @@
+
+public class Counter {
+
+    private int value;
+
+    public Counter(int startValue) {
+        this.value = startValue;
+    }
+    
+    public Counter() {
+        this(0);
+    }
+
+    public void increase() {
+        increase(1);
+    }
+
+    public void decrease() {
+        decrease(1);
+    }
+
+    public void increase(int increaseBy) {
+        if (increaseBy > 0) {
+            this.value = this.value + increaseBy;
+        }
+    }
+
+    public void decrease(int decreaseBy) {
+        if (decreaseBy > 0) {
+            this.value = this.value - decreaseBy;
+        }
+    }
+
+    public int value() {
+        return this.value;
+    }
+}
