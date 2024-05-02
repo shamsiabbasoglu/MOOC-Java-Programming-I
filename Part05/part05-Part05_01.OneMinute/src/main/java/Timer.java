c353fc15f5ce574348c74a9627a6f133cd47f965
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-05-02 12:46:17 +0400

    Introduction to object-oriented programming

---
+++ Part05/part05-Part05_01.OneMinute/src/main/java/Timer.java
@@ -0,0 +1,22 @@
+public class Timer {
+    private final ClockHand seconds;
+    private final ClockHand milliseconds;
+
+    public Timer() {
+        this.seconds = new ClockHand(60);
+        this.milliseconds = new ClockHand(100);
+    }
+    
+    public void advance() {
+        this.milliseconds.advance();
+        
+        if (this.milliseconds.value() == 0) {
+            this.seconds.advance();
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return seconds + ":" + milliseconds;
+    }
+}
