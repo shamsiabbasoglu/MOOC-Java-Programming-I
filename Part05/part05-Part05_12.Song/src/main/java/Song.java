7523d3d9c51b3f5bb937d1d8e283f4f00c6e526b
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-05-09 16:06:02 +0400

    Comparing the equality of objects (equals)

---
+++ Part05/part05-Part05_12.Song/src/main/java/Song.java
@@ -0,0 +1,33 @@
+
+public class Song {
+
+    private String artist;
+    private String name;
+    private int durationInSeconds;
+
+    public Song(String artist, String name, int durationInSeconds) {
+        this.artist = artist;
+        this.name = name;
+        this.durationInSeconds = durationInSeconds;
+    }
+    
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+        if (!(compared instanceof Song)) {
+            return false;
+        }
+        
+        Song comparedSong = (Song) compared;
+        
+        return artist.equals(comparedSong.artist) && name.equals(comparedSong.name) && durationInSeconds == comparedSong.durationInSeconds;
+    }
+
+    @Override
+    public String toString() {
+        return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
+    }
+
+
+}
